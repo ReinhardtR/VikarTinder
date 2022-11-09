f39ae113350b44943f53f212c166d80638ab29e6
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2022-11-09 09:55:28 +0100

    init

---
+++ Persistence/Persistence/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Persistence.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Setup a HTTP/2 endpoint without TLS.
+    options.ListenLocalhost(5287, o => o.Protocols =
+        HttpProtocols.Http2);
+});
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
