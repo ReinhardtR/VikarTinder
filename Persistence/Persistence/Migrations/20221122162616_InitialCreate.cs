cab4a9a1755fa5d425d141a8c3fbed7dc80cfa2e
Ymersod <Simon.99@live.dk>
2022-11-23 10:04:22 +0100

    Ny proto fil til matching

---
+++ Persistence/Persistence/Migrations/20221122162616_InitialCreate.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Substitutes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployerId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Substitutes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Substitutes_Employers_EmployerId",
+                        column: x => x.EmployerId,
+                        principalTable: "Employers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkPosition",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    EmployerId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubstituteId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkPosition", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkPosition_Employers_EmployerId",
+                        column: x => x.EmployerId,
+                        principalTable: "Employers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkPosition_Substitutes_SubstituteId",
+                        column: x => x.SubstituteId,
+                        principalTable: "Substitutes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Substitutes_EmployerId",
+                table: "Substitutes",
+                column: "EmployerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkPosition_EmployerId",
+                table: "WorkPosition",
+                column: "EmployerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkPosition_SubstituteId",
+                table: "WorkPosition",
+                column: "SubstituteId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkPosition");
+
+            migrationBuilder.DropTable(
+                name: "Substitutes");
+
+            migrationBuilder.DropTable(
+                name: "Employers");
+        }
+    }
+}
