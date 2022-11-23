830bc6fd39fb5e1c24c1528cd33e0e5c14e9c473
Milo <miloamadsen@gmail.com>
2022-11-23 10:42:08 +0100

    Refactoring
    ChatServiceReceiver -> ChatServiceServer

--- Persistence/Persistence/Program.cs
+++ Persistence/Persistence/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddDbContext<DataContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<ChatServiceReceiver>();
+app.MapGrpcService<ChatServiceServer>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
