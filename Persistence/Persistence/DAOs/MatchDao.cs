a184845457b039c77b14a77ef4c12ecb745dcb2f
Ymersod <Simon.99@live.dk>
2022-11-30 10:42:16 +0100

    Fix metoder
    persistence

--- Persistence/Persistence/DAOs/MatchDao.cs
+++ Persistence/Persistence/DAOs/MatchDao.cs
@@ -15,18 +15,18 @@ public class MatchDao : IMatchDao
     public async Task<Gig> MatchWithGig(int currentUserId, int matchId)
     {
         //Finder sit eget user domæne objekt
-        Substitute user = await GetSubstituteById(currentUserId);
+        Substitute substitute = await GetSubstituteById(currentUserId);
         
         //Finder domæneobjeketet på ejeren af gigget man har matchet
         Gig gig = await GetGigById(matchId);
         
         
         //Adder den valgte gig sammen med tilhørende employer under sig
-        user.Positions.Add(gig);
+        substitute.Positions.Add(gig);
 
         //Opdateringen og gemning
         _databaseContext.ChangeTracker.Clear();
-        _databaseContext.Substitutes.Update(user);
+        _databaseContext.Substitutes.Update(substitute);
         await _databaseContext.SaveChangesAsync();
 
         return gig;
@@ -34,17 +34,19 @@ public class MatchDao : IMatchDao
 
     public async Task<Substitute> MatchWithSubstitute(int currentUserId, int matchId)
     {
-        Employer employer = await GetEmployerById(matchId);
+        Employer employer = await GetEmployerById(currentUserId);
+        Console.WriteLine(employer.Id + " EMPLOYER");
 
-        Substitute substituteEfc = await GetSubstituteById(currentUserId);
+        Substitute substitute = await GetSubstituteById(matchId);
+        Console.WriteLine(substitute.Id + " SUB");
         
-        employer.Substitutes.Add(substituteEfc);
+        employer.Substitutes.Add(substitute);
         
         _databaseContext.ChangeTracker.Clear();
         _databaseContext.Employers.Update(employer);
         await _databaseContext.SaveChangesAsync();
 
-        return substituteEfc;
+        return substitute;
     }
 
     public async Task<List<Substitute>> GetSubstitutesForMatching(int userId)
@@ -53,12 +55,13 @@ public class MatchDao : IMatchDao
         
         //Listen af substitutes skal være en liste der ikke allerede er swipet nej til før
         //IMPLEMENTERES SENERE
-        
-        List<Substitute>? substitutes = new List<Substitute>();
-        await _databaseContext.Substitutes.ForEachAsync(substitute =>
-            substitutes.Add(substitute));
+        IQueryable<Substitute> subsQuery = _databaseContext.Substitutes.AsQueryable();
 
-        return substitutes;
+        subsQuery = subsQuery.Where(substitute =>
+            substitute.Employers.Any() == false ||
+            substitute.Employers.All(employer => employer.Id != userId) == true);
+
+        return await subsQuery.ToListAsync();
     }
 
     public async Task<List<Gig>> GetGigsForMatching(int id)
@@ -79,6 +82,7 @@ public class MatchDao : IMatchDao
             return await gigsQuery.ToListAsync();
     }
 
+    
     public async Task<Employer> GetEmployerById(int id)
     {
         Employer? employer = await _databaseContext.Employers.FirstOrDefaultAsync(emp =>
