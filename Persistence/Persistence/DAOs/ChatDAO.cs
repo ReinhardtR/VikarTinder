d3713d53ceeb8331bcd713ce5e1475f103d00092
Milo <miloamadsen@gmail.com>
2022-12-01 09:40:56 +0100

    Updated PErsistence

--- Persistence/Persistence/DAOs/ChatDAO.cs
+++ Persistence/Persistence/DAOs/ChatDAO.cs
@@ -16,7 +16,7 @@ public class ChatDAO : IChatDAO
 
     public Task<List<Message>> GetChatHistoryAsync(int chatId)
     {
-        return _dataContext.Messages.Where(m => m.ChatId == chatId).ToListAsync();
+        return _dataContext.Messages.Where(m => m.ChatId == chatId).OrderByDescending(m => m.CreatedAt).ToListAsync();
     }
 
     public async Task<Chat> CreateChatAsync(List<int> userIds)
