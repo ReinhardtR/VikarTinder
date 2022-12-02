8ff1542928c906336c203d96be491e24d52b16a2
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2022-12-02 10:08:00 +0100

    service annotations

--- BusinessServer/src/main/java/com/example/businessserver/logic/JobConfirmationLogicImpl.java
+++ BusinessServer/src/main/java/com/example/businessserver/logic/JobConfirmationLogicImpl.java
@@ -5,22 +5,23 @@ import com.example.businessserver.dtos.chat.JobConfirmation.JobConfirmationAnswe
 import com.example.businessserver.dtos.chat.JobConfirmation.JobConfirmationDTO;
 import com.example.businessserver.services.JobConfirmationServiceClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class JobConfirmationLogicImpl implements JobConfirmationLogic
-{
+@Service
+public class JobConfirmationLogicImpl implements JobConfirmationLogic {
 
-  @Autowired
-  private JobConfirmationServiceClient jobConfirmationServiceClient;
+	@Autowired
+	private JobConfirmationServiceClient jobConfirmationServiceClient;
 
 
-  @Override public JobConfirmationDTO createJobConfirmation(
-      CreateJobConfirmationDTO dto)
-  {
-    return jobConfirmationServiceClient.CreateJobConfirmation(dto);
-  }
+	@Override
+	public JobConfirmationDTO createJobConfirmation(
+					CreateJobConfirmationDTO dto) {
+		return jobConfirmationServiceClient.CreateJobConfirmation(dto);
+	}
 
-  @Override public JobConfirmationDTO answerJobConfirmation(JobConfirmationAnswer dto)
-  {
-    return jobConfirmationServiceClient.answerJobConfirmation(dto);
-  }
+	@Override
+	public JobConfirmationDTO answerJobConfirmation(JobConfirmationAnswer dto) {
+		return jobConfirmationServiceClient.answerJobConfirmation(dto);
+	}
 }
--- BusinessServer/src/main/java/com/example/businessserver/services/implementations/JobConfirmationServiceImpl.java
+++ BusinessServer/src/main/java/com/example/businessserver/services/implementations/JobConfirmationServiceImpl.java
@@ -6,32 +6,32 @@ import com.example.businessserver.dtos.chat.JobConfirmation.CreateJobConfirmatio
 import com.example.businessserver.dtos.chat.JobConfirmation.JobConfirmationAnswer;
 import com.example.businessserver.dtos.chat.JobConfirmation.JobConfirmationDTO;
 import com.example.businessserver.services.JobConfirmationServiceClient;
-import com.example.businessserver.services.factories.ChatServiceFactory;
 import com.example.businessserver.services.factories.JobConfirmationServiceFactory;
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.springframework.stereotype.Service;
 
-public class JobConfirmationServiceImpl implements JobConfirmationServiceClient
-{
-  @GrpcClient("grpc-server")
-  private JobConfirmationServiceGrpc.JobConfirmationServiceBlockingStub jobConfirmationServiceBlockingStub;
+@Service
+public class JobConfirmationServiceImpl implements JobConfirmationServiceClient {
+	@GrpcClient("grpc-server")
+	private JobConfirmationServiceGrpc.JobConfirmationServiceBlockingStub jobConfirmationServiceBlockingStub;
 
 
-  @Override public JobConfirmationDTO CreateJobConfirmation(
-      CreateJobConfirmationDTO dto)
-  {
-    CreateJobConfirmationRequest request = JobConfirmationServiceFactory.toCreateJobConfirmationRequest(dto);
-    CreateJobConfirmationResponse response = jobConfirmationServiceBlockingStub.createJobConfirmation(request);
+	@Override
+	public JobConfirmationDTO CreateJobConfirmation(
+					CreateJobConfirmationDTO dto) {
+		CreateJobConfirmationRequest request = JobConfirmationServiceFactory.toCreateJobConfirmationRequest(dto);
+		CreateJobConfirmationResponse response = jobConfirmationServiceBlockingStub.createJobConfirmation(request);
 
-    return JobConfirmationServiceFactory.toJobConfirmationDTO(response);
+		return JobConfirmationServiceFactory.toJobConfirmationDTO(response);
 
-  }
+	}
 
-  @Override public JobConfirmationDTO answerJobConfirmation(JobConfirmationAnswer dto)
-  {
-    JobConfirmationAnswerRequest request = JobConfirmationServiceFactory.toJobConfirmationAnswerRequest(dto);
-    JobConfirmationAnswerResponse response = jobConfirmationServiceBlockingStub.answerJobConfirmation(request);
+	@Override
+	public JobConfirmationDTO answerJobConfirmation(JobConfirmationAnswer dto) {
+		JobConfirmationAnswerRequest request = JobConfirmationServiceFactory.toJobConfirmationAnswerRequest(dto);
+		JobConfirmationAnswerResponse response = jobConfirmationServiceBlockingStub.answerJobConfirmation(request);
 
-    return JobConfirmationServiceFactory.toJobConfirmationDTO(response);
+		return JobConfirmationServiceFactory.toJobConfirmationDTO(response);
 
-  }
+	}
 }
