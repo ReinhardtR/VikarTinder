4b9f80ed67b24438bce20d86218001d044f9f0b9
Milo "The Shmeat" Madsen <miloamadsen@gmail.com>
2022-12-02 17:42:26 +0100

    vscodediff

--- Persistence/Persistence/DAOs/IJobConfirmationDAO.cs
+++ Persistence/Persistence/DAOs/IJobConfirmationDAO.cs
@@ -8,5 +8,4 @@ public interface IJobConfirmationDAO
     Task<JobConfirmation> CreateJobConfirmationAsync(int requestChatId, int requestSubstituteId, int requestEmployerId);
     Task<JobConfirmation?> AnswerJobConfirmationAsync(int requestId, int requestChatId, bool requestIsAccepted);
     Task<JobConfirmation?> GetJobConfirmationAsync(int requestId);
-
 }
