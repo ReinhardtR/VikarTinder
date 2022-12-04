894913ee4c6a1364f2a2aa2f15fd4f53f6772660
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2022-12-04 14:46:28 +0100

    use snackbar action errors

--- WebClient/WebClient/Program.cs
+++ WebClient/WebClient/Program.cs
@@ -2,6 +2,7 @@ using HttpClients;
 using HttpClients.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
 using MudBlazor.Services;
 using WebClient;
 using WebSockets;
@@ -18,6 +19,13 @@ builder.Services.AddScoped<JobConfirmationService>();
 builder.Services.AddScoped<Client>();
 builder.Services.AddScoped<ChatSocket>();
 
-builder.Services.AddMudServices();
+builder.Services.AddMudServices((config) =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 100;
+    config.SnackbarConfiguration.ShowTransitionDuration = 100;
+});
 
 await builder.Build().RunAsync();
