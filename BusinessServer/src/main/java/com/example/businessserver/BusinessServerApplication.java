939385c56893ae014e52acac78842c7b98ae5d94
Simon S.C <95408442+Lemske@users.noreply.github.com>
2022-12-08 09:16:54 +0100

    Setup Auth
    Hope its gonna work

--- BusinessServer/src/main/java/com/example/businessserver/BusinessServerApplication.java
+++ BusinessServer/src/main/java/com/example/businessserver/BusinessServerApplication.java
@@ -1,10 +1,13 @@
 package com.example.businessserver;
 
+import com.example.businessserver.restcontrollers.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class BusinessServerApplication {
 
 	public static void main(String[] args) {
